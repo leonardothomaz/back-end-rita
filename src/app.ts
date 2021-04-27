b9b3278cf8681ff58a2121908409f78f421ede1d
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2021-04-26 23:05:44 -0300

    Setting up the test automation structure

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv-safe";
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+
+import routes from "./routes";
+
+import "./database";
+import AppError from "./errors/AppError";
+
+dotenv.config({
+  allowEmptyValues: true,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+export { app };
