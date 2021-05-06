990c962e5791939e14f9975863898986b667d470
icompsoftleonardo <leonardo.thomaz@icompsoft.com.br>
2021-05-05 22:43:17 -0300

    Corrigido o teste para que o mesmo passe perfeitamente, algumas alterações no ormconfig são temporárias

--- ormconfig.js
+++ ormconfig.js
@@ -5,8 +5,8 @@ module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   connectionString: process.env.DATABASE_URL,
-  entities: [__dirname + `/${compiled ? "dist" : "src"}/models/*{.ts,.js}`],
-  migrations: [`./${compiled ? "dist" : "src"}/database/migrations/*{.ts,.js}`],
+  entities: [__dirname + `/src/models/*{.ts,.js}`],
+  migrations: [__dirname + `/src/database/migrations/*{.ts,.js}`],
   cli: {
     migrationsDir: `./src/database/migrations/`,
     entitiesDir: `src/models`,
--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -6,7 +6,7 @@ describe("users", () => {
   it("Should not be able to create a new user without the user password", async () => {
     const response = await request(app).post("/users").send({
       email: "test@test.com",
-      type: 0,
+      type: '0',
     });
 
     expect(response.status).toBe(500);
@@ -15,7 +15,7 @@ describe("users", () => {
   it("Should not be able to create a new user without the user e-mail", async () => {
     const response = await request(app).post("/users").send({
       password: "123",
-      type: 0,
+      type: '0',
     });
 
     expect(response.status).toBe(500);
@@ -34,17 +34,17 @@ describe("users", () => {
     const response = await request(app).post("/users").send({
       email: "teste@test.com",
       password: "123",
-      type: 0,
+      type: '0',
     });
 
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(200);
   });
 
   it("Should not be able to create a new user with exists email", async () => {
     const response = await request(app).post("/users").send({
       email: "teste@test.com",
       password: "123",
-      type: 0,
+      type: '0',
     });
 
     expect(response.status).toBe(400);
