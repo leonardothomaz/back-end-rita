2de89323be2020d27b49a66ab106d9abea8aebd5
icompsoftleonardo <leonardo.thomaz@icompsoft.com.br>
2021-07-27 08:27:00 -0300

    docs: Adicionada documentação de rotas da API com Swagger

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
+
 import routes from "./routes";
 
 import "./database";
@@ -17,6 +20,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -26,6 +32,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   } else {
+    console.log(err);
+    
     return response.status(500).json({
       status: "error",
       message: "Internal server error",
